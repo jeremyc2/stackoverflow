8c762f7399dc317996f2c169ad5bf954e68cd19f
Jeremy Chandler <goldenmeso@gmail.com>
2022-03-19 05:54:51 -0500

    header component

---
+++ components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import classNames from 'classnames'
+import styles from './header.module.scss'
+
+export default function Header() {
+    return <div className={classNames(styles.header)}>
+
+    </div>
+}
--- postcss.config.js
+++ postcss.config.js
@@ -19,7 +19,8 @@ module.exports = {
     [
       'postcss-jit-props',
       {
-        ...OpenProps
+        ...OpenProps,
+        custom_selector: ':global'
       }
     ],
   ]
